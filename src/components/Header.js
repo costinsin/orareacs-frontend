dffdcce7cde123654171c4519cf026f6356c5a62
Costin-Robert Sin <sin.costinrobert@gmail.com>
2022-04-21 01:08:36 +0300

    [header] Style header depending on the login info
    Signed-off-by: Costin-Robert Sin <sin.costinrobert@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,26 +2,77 @@ import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
-export default function Header() {
-  return (
-    <Navbar bg="light" expand="lg">
-      <LinkContainer style={{ marginLeft: "1rem" }} to="/">
-        <Navbar.Brand>Orare ACS</Navbar.Brand>
-      </LinkContainer>
-      <Navbar.Toggle
-        aria-controls="basic-navbar-nav"
-        style={{ marginRight: "1rem" }}
-      />
-      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-        <Nav>
-          <LinkContainer
-            style={{ paddingRight: "2rem", paddingLeft: "1rem" }}
-            to="/auth"
-          >
-            <Nav.Link>Login</Nav.Link>
+export default function Header({ userType }) {
+  switch (userType) {
+    case "admin":
+      return (
+        <Navbar bg="light" expand="lg">
+          <LinkContainer style={{ marginLeft: "1rem" }} to="/">
+            <Navbar.Brand>Orare ACS</Navbar.Brand>
           </LinkContainer>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+          <Navbar.Toggle
+            aria-controls="basic-navbar-nav"
+            style={{ marginRight: "1rem" }}
+          />
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-end"
+          >
+            <NavButton text="Manage" path="/manage" />
+            <NavButton text="Settings" path="/settings" />
+            <NavButton text="Log out" path="/logout" />
+          </Navbar.Collapse>
+        </Navbar>
+      );
+    case "student":
+      return (
+        <Navbar bg="light" expand="lg">
+          <LinkContainer style={{ marginLeft: "1rem" }} to="/">
+            <Navbar.Brand>Orare ACS</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle
+            aria-controls="basic-navbar-nav"
+            style={{ marginRight: "1rem" }}
+          />
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-end"
+          >
+            <NavButton text="Settings" path="/settings" />
+            <NavButton text="Log out" path="/logout" />
+          </Navbar.Collapse>
+        </Navbar>
+      );
+    default:
+      return (
+        <Navbar bg="light" expand="lg">
+          <LinkContainer style={{ marginLeft: "1rem" }} to="/">
+            <Navbar.Brand>Orare ACS</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle
+            aria-controls="basic-navbar-nav"
+            style={{ marginRight: "1rem" }}
+          />
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-end"
+          >
+            <NavButton text="Sign up / Sign in" path="/auth" />
+          </Navbar.Collapse>
+        </Navbar>
+      );
+  }
+}
+
+function NavButton({ text, path }) {
+  return (
+    <Nav>
+      <LinkContainer
+        style={{ paddingRight: "2rem", paddingLeft: "1rem" }}
+        to={path}
+      >
+        <Nav.Link>{text}</Nav.Link>
+      </LinkContainer>
+    </Nav>
   );
 }
